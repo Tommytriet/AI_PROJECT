55e7a2ca2d0965051f5965c67f2af6131914b9ea
Tommytriet <115491597+Tommytriet@users.noreply.github.com>
2025-06-11 21:45:28 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: " https://tommytriet.github.io",
+	site: "https://tommytriet.github.io",
 	base: "/AI_project",
 	trailingSlash: "always",
 	integrations: [
